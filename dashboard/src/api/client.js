12666a41f1014bcec8d829860a42f67e4ba88dde
chnihal <chetlapellinihal@gmail.com>
2026-06-02 14:07:35 +0530

    Update production URLs

--- dashboard/src/api/client.js
+++ dashboard/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3002",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://zerodha-backend-s52h.onrender.com",
   withCredentials: true,
 });
 
